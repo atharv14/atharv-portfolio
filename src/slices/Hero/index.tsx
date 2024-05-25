3d6c6383233532325ecd865469804dc24bf55a33
atharv14 <mhatreatharv94@gmail.com>
2024-05-25 02:48:17 -0400

    Added icon and Fixed text resizing

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -83,16 +83,16 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
     <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
       <Shapes />
       <div className="col-start-1 md:row-start-1">
-        <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none
+        <h1 className="mb-8 font-extrabold leading-none
       tracking-tighter"
           aria-label={
             slice.primary.first_name + " " + slice.primary.last_name
           }
         >
-          <span className="block text-slate-300">
+          <span className="block text-slate-300 text-[clamp(3rem,12vmin,20rem)]">
             {renderLetters(slice.primary.first_name, "first")}
           </span>
-          <span className="-mt-[.2em] block text-slate-500">
+          <span className="-mt-[.2em] block text-slate-500 text-[clamp(3rem,12vmin,20rem)]">
             {renderLetters(slice.primary.last_name, "last")}
           </span>
         </h1>
